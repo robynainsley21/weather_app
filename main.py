8b3c2fc92757298dcb3dee9e0425b2793912a673
nusaibah1 <charifnusaibah01@gmail.com>
2024-12-03 16:06:09 +0200

    error handeling

--- main.py
+++ main.py
@@ -3,30 +3,34 @@ import requests
 
 app = Flask(__name__)
 
+#  OpenWeather API key
 API_key = "0a05aaca142eafd2174374fa14f28035"
 
 @app.route('/')
 def index():
-    return render_template('index.html')
+ return render_template('index.html')
 
 @app.route('/weather', methods=['POST'])
-def weather():
-    city = request.form['city']
-    url = f"http://api.openweathermap.org/data/2.5/weather?q={city}&appid={API_key}&units=metric"
-    response = requests.get(url).json()
+def get_weather():
+ city = request.form['city']
+ url = f'http://api.openweathermap.org/data/2.5/weather?q={city}&appid={API_key}&units=metric'
+ response = requests.get(url).json()
 
-    if response.get('cod') != 200:
-        return render_template('index.html', error="City not found. Please try again.")
+# Validating user input: if city code not found error message is returned
+ if response.get('cod') != 200:
+     return render_template('index.html', error="City not found.")
 
-    weather_data = {
-        'city': city,
-        'temperature': response['main']['temp'],
-        'description': response['weather'][0]['description'],
-        'icon': response['weather'][0]['icon'],
-        'wind_speed': response['wind']['speed'],    
-        'humidity': response['main']['humidity']
-    }
-    return render_template('weather.html', weather_data=weather_data)
+ weather_data = {
+     'city': city,
+     'temperature': response['main']['temp'],
+     'description': response['weather'][0]['description'],
+     'humidity': response['main']['humidity'],
+     'wind_speed': response['wind']['speed'],
+     'pressure': response['main']['pressure'],
+     'icon': response['weather'][0]['icon']
+
+ }
+ return render_template('weather.html', weather_data=weather_data)
 
 if __name__ == '__main__':
-    app.run(debug=True)
+ app.run(debug=True)
