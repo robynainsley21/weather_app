99d8180a98a287a549f2348d423a72131fdd95f6
Robyn Carnow <robynainsley2i@gmail.com>
2024-12-03 09:40:30 +0200

    Integrate API

--- main.py
+++ main.py
@@ -13,5 +13,20 @@ def index():
 def weather():
     city = request.form['city']
     url = f"http://api.openweathermap.org/data/2.5/weather?q={city}&appid={API_key}&units=metric"
-    response = requests.get(url)
-    data = response.json()
+    response = requests.get(url).json()
+
+    if response.get('cod') != 200:
+        return render_template('index.html', error="City not found. Please try again.")
+
+    weather_data = {
+        'city': city,
+        'temperature': response['main']['temp'],
+        'description': response['weather'][0]['description'],
+        'icon': response['weather'][0]['icon'],
+        'wind_speed': response['wind']['speed'],    
+        'humidity': response['main']['humidity']
+    }
+    return render_template('weather.html', weather_data=weather_data)
+
+if __name__ == '__main__':
+    app.run(debug=True)
